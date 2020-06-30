cfab40d55e28c7d6febd47c9af957a58bd4ef2ef
Hidetaka Okamoto <info@wp-kyoto.net>
2020-06-30 12:32:05 +0900

    add cap examples

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+// Call the element loader after the app has been rendered the first time
+defineCustomElements(window);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,7 @@
 import MessageListItem from '../components/MessageListItem';
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+import { useClipboard, availableFeatures } from '@ionic/react-hooks/clipboard';
+import { Plugins, ClipboardReadResult } from '@capacitor/core';
 import { Message, getMessages } from '../data/messages';
 import {
   IonContent,
@@ -10,12 +12,33 @@ import {
   IonRefresherContent,
   IonTitle,
   IonToolbar,
-  useIonViewWillEnter
+  useIonViewWillEnter,
+  IonInput,
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardTitle
 } from '@ionic/react';
+import { Plugins as CapPlugins } from '@capacitor/core';
 import './Home.css';
+
+const useToaster = () => {
+  const { Toast } = CapPlugins;
+  const showToast = useCallback(() => {
+    Toast.show({
+      text: 'Hello!' + Math.random()
+    });
+  }, [])
+  return {
+    showToast
+  }
+}
 
 const Home: React.FC = () => {
 
+  const capHooks = useClipboard()
+  const { showToast } = useToaster()
   const [messages, setMessages] = useState<Message[]>([]);
 
   useIonViewWillEnter(() => {
@@ -48,6 +71,28 @@ const Home: React.FC = () => {
             </IonTitle>
           </IonToolbar>
         </IonHeader>
+        <IonContent>
+          <IonCard>
+            <IonCardHeader>
+              Toast
+            </IonCardHeader>
+            <IonCardContent>
+              <IonButton onClick={showToast}>Show toast</IonButton>
+            </IonCardContent>
+          </IonCard>
+          <IonCard>
+            <IonCardHeader>
+              <IonCardTitle>Use Capacitor</IonCardTitle>
+            </IonCardHeader>
+            {availableFeatures.useClipboard ? (
+            <IonCardContent>
+              <pre>{JSON.stringify(capHooks.value, null, 2)}</pre>
+              <IonButton onClick={() => capHooks.setValue('Hello Ionic' + Math.random())}>Write</IonButton>
+              <IonButton onClick={() => capHooks.getValue()}>Read</IonButton>
+            </IonCardContent>
+            ) : null}
+          </IonCard>
+        </IonContent>
 
         <IonList>
           {messages.map(m => <MessageListItem key={m.id} message={m} />)}
